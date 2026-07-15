6d008a034954d305f1274992e277a993484fb9cc
joao.teodoro <joao.teodoro1610@gmail.com>
2026-07-15 11:13:02 -0300

    test: Initialize CreateUserUsecaseTests

--- Iron/tests/Iron.Aplication.Tests/Usecases/Auth/CreateUserUsecaseTests.cs
+++ Iron/tests/Iron.Aplication.Tests/Usecases/Auth/CreateUserUsecaseTests.cs
@@ -1,6 +1,29 @@
+using FluentValidation;
+using Iron.Aplication.DTOs;
+using Iron.Aplication.Usecases.Auth;
+using Iron.Domain.Repositories;
+using Iron.Domain.Security;
+using NSubstitute;
+
 namespace Iron.Aplication.Tests.Usecases.Auth;
 
 public class CreateUserUsecaseTests
 {
+    private readonly CreateUserUsecase _sut;
 
+    public CreateUserUsecaseTests()
+    {
+        var userRepository = Substitute.For<IUserRepository>();
+        var unitOfWork = Substitute.For<IUnitOfWork>();
+        var passwordHasher = Substitute.For<IPasswordHasher>();
+        var validator = Substitute.For<IValidator<CreateUserRequest>>();
+
+        _sut = new CreateUserUsecase(userRepository, unitOfWork, passwordHasher, validator);
+    }
+
+    [Fact]
+    public async Task ExecuteAsync_GivenCreateUserRequest_ShouldReturnOkResultAndCreateUser()
+    {
+
+    }
 }
